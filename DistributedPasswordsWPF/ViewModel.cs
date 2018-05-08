d2a86ffa11c760c21c25b8d1edcdd61e734a6544
Philipp Hock <mail@philipphock.rocks>
2018-05-08 22:12:04 +0200

    added all pages to viewmodel

--- DistributedPasswordsWPF/ViewModel.cs
+++ DistributedPasswordsWPF/ViewModel.cs
@@ -9,13 +9,14 @@ namespace DistributedPasswordsWPF
 {
     class ViewModel
     {
-        public enum Pages { Main, Unlock, EditNew };
+        public enum Pages { Main, Unlock, EditNew, ChangePW };
         private Dictionary<Pages, Page> pagesLUT = new Dictionary<Pages, Page>();
         
         private MainWindow window;
         private readonly Page mainView;
         private readonly Page unlock;
         private readonly Page editnew;
+        private readonly Page changepw;
 
 
         private ViewModel()
@@ -27,6 +28,7 @@ namespace DistributedPasswordsWPF
             pagesLUT.Add(Pages.Main, mainView);
             pagesLUT.Add(Pages.Unlock, unlock);
             pagesLUT.Add(Pages.EditNew, editnew);
+            pagesLUT.Add(Pages.ChangePW, changepw);
 
         }
 
