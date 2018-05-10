948be37003680108e4ffc385e17db18db3a0133c
Philipp Hock <mail@philipphock.de>
2018-05-10 14:50:50 +0200

    db++

--- DistributedPasswordsWPF/model/Settings.cs
+++ DistributedPasswordsWPF/model/Settings.cs
@@ -13,6 +13,10 @@ namespace DistributedPasswordsWPF.model
         private static string SETTINGS = "assets/db/settings.sqlite";
         private static string SETTINGS_DIR = "assets/db/";
 
+        private static string DB_DIR = "";
+        private static string KEYS_DIR = "";
+
+
         static Settings()
         {
             
@@ -20,15 +24,70 @@ namespace DistributedPasswordsWPF.model
 
         public static void Init()
         {
+            bool init = false;
             if (!System.IO.Directory.Exists(SETTINGS_DIR))
             {
+                init = true;
                 System.IO.Directory.CreateDirectory(SETTINGS_DIR);
             }
             
             SQLiteConnection.CreateFile(SETTINGS);
             SQLiteConnection dbConnection = new SQLiteConnection("Data Source = " + SETTINGS + "; Version = 3;");
             dbConnection.Open();
+
+            if (init)
+            {
+
+                string sql = @"
+                    CREATE TABLE `settings` (
+	                    `ID`	INTEGER NOT NULL UNIQUE,
+	                    `KEY`	TEXT NOT NULL,
+	                    `VALUE`	TEXT,
+	                    PRIMARY KEY(`ID`)
+                    );
+                ";
+                
+                SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+                command.ExecuteNonQuery();
+
+                string sql2 = @"
+                    #INSERT INTO `settings`(`ID`,`KEY`,`VALUE`) VALUES (1,'',NULL);
+                ";
+
+                SQLiteCommand command2 = new SQLiteCommand(sql2, dbConnection);
+                command2.ExecuteNonQuery();
+
+            }
+
+            
             Debug.WriteLine("init");
+        }
+
+
+        public static string KEYS_PATH
+        {
+            get
+            {
+                return KEYS_DIR;
+            }
+
+            set
+            {
+                KEYS_DIR = value;
+            }
+        }
+
+        public static string DB_PATH
+        {
+            get
+            {
+                return DB_DIR;
+            }
+
+            set
+            {
+                DB_DIR = value;
+            }
         }
     }
 }
