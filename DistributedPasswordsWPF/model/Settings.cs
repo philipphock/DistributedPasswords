e739470394a51c6ad843f7487bcbe3ecf73180f6
Philipp Hock <mail@philipphock.de>
2018-05-10 17:59:19 +0200

    db++

--- DistributedPasswordsWPF/model/Settings.cs
+++ DistributedPasswordsWPF/model/Settings.cs
@@ -10,11 +10,44 @@ namespace DistributedPasswordsWPF.model
 {
     class Settings
     {
-        private static string SETTINGS = "assets/db/settings.sqlite";
-        private static string SETTINGS_DIR = "assets/db/";
+        private static string SETTINGS_DIR = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "DstPassword");
+        private static string SETTINGS = System.IO.Path.Combine(SETTINGS_DIR,"settings.sqlite");
 
-        private static string DB_DIR = "";
-        private static string KEYS_DIR = "";
+        private static SQLiteConnection dbConnection;
+
+        private static string DB_DIR = null;
+        private static string KEYS_DIR = null;
+
+
+        private static readonly string CREATE_TABLE = @"
+                    CREATE TABLE `settings` (
+	                    `ID`	INTEGER NOT NULL UNIQUE,
+	                    `KEY`	TEXT NOT NULL,
+	                    `VALUE`	TEXT,
+	                    PRIMARY KEY(`ID`)
+                    );
+                ";
+
+        private static readonly string CREATE_KV_ENTRIES = @"
+                    INSERT INTO `settings`(`ID`,`KEY`,`VALUE`) VALUES (1,'DB',NULL);
+                    INSERT INTO `settings`(`ID`,`KEY`,`VALUE`) VALUES (2,'KEYS',NULL);
+                ";
+
+        private static readonly string UPDATE_DB = @"
+            UPDATE `settings` SET `VALUE`=? WHERE _rowid_='1';
+        ";
+
+        private static readonly string UPDATE_KEYS = @"
+            UPDATE `settings` SET `VALUE`=? WHERE _rowid_='2';
+        ";
+
+        private static readonly string SELECT_DB = @"
+            SELECT `VALUE` FROM `settings` WHERE `ID` = 1;        
+        ";
+
+        private static readonly string SELECT_KEYS = @"
+            SELECT `VALUE` FROM `settings` WHERE `ID` = 2;        
+        ";
 
 
         static Settings()
@@ -25,43 +58,50 @@ namespace DistributedPasswordsWPF.model
         public static void Init()
         {
             bool init = false;
+            Debug.WriteLine(SETTINGS);
             if (!System.IO.Directory.Exists(SETTINGS_DIR))
             {
                 init = true;
                 System.IO.Directory.CreateDirectory(SETTINGS_DIR);
+                SQLiteConnection.CreateFile(SETTINGS);
+
             }
-            
-            SQLiteConnection.CreateFile(SETTINGS);
-            SQLiteConnection dbConnection = new SQLiteConnection("Data Source = " + SETTINGS + "; Version = 3;");
+
+            dbConnection = new SQLiteConnection("Data Source = " + SETTINGS + "; Version = 3;");
             dbConnection.Open();
 
             if (init)
             {
 
-                string sql = @"
-                    CREATE TABLE `settings` (
-	                    `ID`	INTEGER NOT NULL UNIQUE,
-	                    `KEY`	TEXT NOT NULL,
-	                    `VALUE`	TEXT,
-	                    PRIMARY KEY(`ID`)
-                    );
-                ";
-                
-                SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+                Debug.WriteLine("create table");
+
+                SQLiteCommand command = new SQLiteCommand(CREATE_TABLE, dbConnection);
+                SQLiteCommand command2 = new SQLiteCommand(CREATE_KV_ENTRIES, dbConnection);
                 command.ExecuteNonQuery();
-
-                string sql2 = @"
-                    #INSERT INTO `settings`(`ID`,`KEY`,`VALUE`) VALUES (1,'',NULL);
-                ";
-
-                SQLiteCommand command2 = new SQLiteCommand(sql2, dbConnection);
                 command2.ExecuteNonQuery();
 
             }
+            else
+            {
+                SQLiteCommand command = new SQLiteCommand(SELECT_DB, dbConnection);
+                SQLiteCommand command2 = new SQLiteCommand(SELECT_KEYS, dbConnection);
+                SQLiteDataReader reader1 = command.ExecuteReader();
+                while (reader1.Read())
+                {
+                    DB_PATH = reader1["VALUE"].ToString();
+                }
+                SQLiteDataReader reader2 = command2.ExecuteReader();
+                while (reader2.Read())
+                {
+                    KEYS_PATH = reader2["VALUE"].ToString();
+                }
+            }
 
-            
+
             Debug.WriteLine("init");
         }
+
+        
 
 
         public static string KEYS_PATH
@@ -73,7 +113,11 @@ namespace DistributedPasswordsWPF.model
 
             set
             {
+                SQLiteCommand command = new SQLiteCommand(UPDATE_KEYS, dbConnection);
+                command.
+
                 KEYS_DIR = value;
+
             }
         }
 
@@ -89,5 +133,7 @@ namespace DistributedPasswordsWPF.model
                 DB_DIR = value;
             }
         }
+
+
     }
 }
