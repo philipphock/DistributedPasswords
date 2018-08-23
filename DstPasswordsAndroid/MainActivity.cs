b675ddf04094849cb9f979cf53a30b4a736cf2c9
Philipp Hock <mail@philipphock.rocks>
2018-08-23 19:37:04 +0200

    android++

--- DstPasswordsAndroid/MainActivity.cs
+++ DstPasswordsAndroid/MainActivity.cs
@@ -2,18 +2,50 @@
 using Android.Widget;
 using Android.OS;
 using Android.Support.V7.App;
+using Android.Content;
 
 namespace DstPasswordsAndroid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        public enum Pages
+        {
+            Unlock
+        }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            _route(Pages.Unlock);
+
+            
+        }
+
+        private void _route(Pages p)
+        {
+            System.Type page = null;
+            switch (p)
+            {
+                case Pages.Unlock:
+                    page = typeof(Unlock);
+                    break;
+            }
+            
+
+            if (page != null)
+            {
+                Intent intent = new Intent(this, page);
+                StartActivity(intent);
+            }
+            else
+            {
+                Toast.MakeText(Application.Context, "No activity found", ToastLength.Short);
+            }
+            
         }
     }
 }
--- DstPasswordsAndroid/Unlock.cs
+++ DstPasswordsAndroid/Unlock.cs
@@ -6,6 +6,7 @@ using System.Text;
 using Android.App;
 using Android.Content;
 using Android.OS;
+using Android.Preferences;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
@@ -15,12 +16,58 @@ namespace DstPasswordsAndroid
     [Activity(Label = "Unlock")]
     public class Unlock : Activity
     {
+        private readonly string KEY_DB = "DB";
+        private readonly string KEY_KEYS = "KEYS";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            
 
+            SetContentView(Resource.Layout.Unlock);
+            _loadValues();
             // Create your application here
         }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            _storeValues();
+        }
+
+        private void _loadValues()
+        {
+            EditText keysUIField = FindViewById<EditText>(Resource.Id.keys);
+            EditText dbUIField = FindViewById<EditText>(Resource.Id.db);
+
+            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
+            var keys = prefs.GetString(KEY_KEYS,"");
+            var db = prefs.GetString(KEY_DB, "");
+
+            keysUIField.Text = keys;
+            dbUIField.Text = db;
+        }
+
+        private void _storeValues()
+        {
+            EditText keysUIField = FindViewById<EditText>(Resource.Id.keys);
+            EditText dbUIField = FindViewById<EditText>(Resource.Id.db);
+
+            var keys = keysUIField.Text;
+            var db = dbUIField.Text;
+            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
+            ISharedPreferencesEditor editor = prefs.Edit();
+            editor.PutString(KEY_DB, db);
+            editor.PutString(KEY_KEYS, keys);
+            
+            editor.Apply();        // applies changes asynchronously on newer APIs
+            
+
+        }
+
+        public void UnlockBtn(View v)
+        {
+            
+        }
+
     }
 }
